d7886d414f81d1fa42015a9cab37a0b3d48803db
Виктор Сагалаков <sagalakov.viktor@gmail.com>
2025-09-20 21:19:57 +0700

    fixed

--- src/utils/validator.py
+++ src/utils/validator.py
@@ -3,6 +3,7 @@ from typing import List
 
 
 def validate_files_exist(file_paths: List) -> None:
+    """Проверяет существуют ли файлы"""
     for file_path in file_paths:
         if not os.path.exists(file_path):
             raise FileNotFoundError(f"Файл '{file_path}' не найден")
